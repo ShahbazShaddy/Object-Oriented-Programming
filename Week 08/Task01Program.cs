d698b2548795ff99ffb7e423ab57ff4f334cd4fa
Shahbaz Ghafil <121121677+ShahbazShaddy@users.noreply.github.com>
2023-06-10 10:51:12 -0700

    Add files via upload

---
+++ Week 08/Task01Program.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab08
+{
+    internal class Program
+    {
+
+        static void Main(string[] args)
+        {
+            Cylinder cylinder1 = new Cylinder();
+            cylinder1.setHeight(1.0);
+            Console.WriteLine( "Volume 1:"+cylinder1.getVolume());
+            Cylinder cylinder2 = new Cylinder(1.0,1.0);
+            Console.WriteLine( "Volume 2:"+cylinder2.getVolume());
+            Cylinder cylinder3 = new Cylinder(1.0, 2.0);
+            Console.WriteLine( "Volume 3:"+cylinder3.getVolume());
+            Console.WriteLine(cylinder1.toString());
+            Console.ReadKey();
+        }
+
+    }
+
+    class Circle
+    {
+        protected double radius;
+        protected string color;
+
+        public Circle()
+        {
+            radius = 1.0;
+            color = "red";
+        }
+        public Circle(double radius)
+        {
+            this.radius = radius;
+        }
+        public Circle(double radius, string color)
+        {
+            this.radius = radius;
+            this.color = color;
+        }
+        public void setRadius(double radius)
+        {
+            this.radius = radius;
+        }
+        public double getRadius()
+        {
+            return radius;
+        }
+        public void setColor(string color)
+        {
+            this.color = color;
+        }
+        public string getColor()
+        {
+            return color;
+        }
+        public double getArea()
+        {
+            double area = Math.PI * (Math.Pow(radius, 2));
+            return area;
+        }
+
+        public String toString()
+        {
+            return "Circle [radius=" + radius + ", color=" + color + "]";
+        }
+
+    }
+    class Cylinder : Circle
+    {
+        private double height;
+        public Cylinder()
+        {
+            height = 1.0;
+        }
+        public Cylinder(double height)
+        {
+            this.height = height;
+        }
+        public Cylinder(double height,double radius)
+        {
+            this.radius = radius;
+            this.height = height;
+        }
+        public Cylinder(double height, double radius,string color)
+        {
+            this.radius = radius;
+            this.height = height;
+            this.color = color;
+        }
+        public double getHeight()
+        {
+            return height;
+        }
+        public void setHeight(double height)
+        {
+            this.height = height;
+        }
+        public double getVolume()
+        {
+            double volume= base.getArea()* height;
+            return volume;
+        }
+    }
+}
---
+++ Week 08/Task02Program.cs
@@ -0,0 +1,141 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab_08_b
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Create a Person object
+            Person person = new Person("Ali", "123 Main Street");
+            Console.WriteLine(person.ToString());
+
+            // Create a Student object
+            Student student = new Student("Saboor", "456 Side Street", "Computer Science", 2023, 2000.0);
+            Console.WriteLine(student.ToString());
+
+            // Create a Staff object
+            Staff staff = new Staff("Nomi", "78 Jinnah Street", "ABC School", 5000.0);
+            Console.WriteLine(staff.ToString());
+
+            // Modify properties using getters and setters
+            student.setProgram("Information Technology");
+            student.setYear(2022);
+            student.setFee(2500.0);
+
+            staff.setSchool("XYZ University");
+            staff.setPay(6000.0);
+
+            // Print updated objects
+            Console.WriteLine(student.ToString());
+            Console.WriteLine(staff.ToString());
+            Console.ReadKey();
+        }
+    }
+    class Person
+    {
+        protected string name;
+        protected string address;
+        public Person(string name, string address)
+        {
+            this.name = name;
+            this.address = address;
+        }
+        public string getName()
+        {
+            return name;
+        }
+        public string getAddress()
+        {
+            return address;
+        }
+        public void setAddress(string address)
+        {
+            this.address = address;
+        }
+        public virtual String toString()
+        {
+            return "Person[Name=" + name + "Address" + address + "]";
+        }
+    }
+    class Student : Person
+    {
+        private string program;
+        private int year;
+        private double fee;
+        public Student(string name, string address, string program, int year, double fee)
+        : base(name, address)
+        {
+            this.program = program;
+            this.year = year;
+            this.fee = fee;
+            this.name = name;
+            this.address = address;
+        }
+        public string getProgram()
+        {
+            return program;
+        }
+        public void setProgram(string program)
+        {
+            this.program = program;
+        }
+        public int getYear()
+        {
+            return year;
+        }
+        public void setYear(int year)
+        {
+            this.year = year;
+        }
+        public double getFee()
+        {
+            return fee;
+        }
+        public void setFee(double fee)
+        {
+            this.fee = fee;
+        }
+        public override string ToString()
+        {
+            return "Student[Person[name=" + name + ", Address=" + address + "]," +
+                " program=" + program + ", year=" + year + ", fee=" + fee + "]";
+        }
+    }
+    class Staff:Person
+    {
+        private string school;
+        private double pay;
+        public Staff(string name, string address, string school, double pay):base(name,address)
+        {
+            this.name = name;
+            this.address = address;
+            this.school = school;
+            this.pay = pay;
+        }
+        public string getSchool()
+        {
+            return school;
+        }
+        public void setSchool(string school)
+        {
+            this.school = school;
+        }
+        public double getPay()
+        {
+            return pay;
+        }
+        public void setPay(double pay)
+        {
+            this.pay = pay;
+        }
+        public override string ToString()
+        {
+            return "Staff[Person[name=" + name + ", Address=" + address + "], school=" + school + ", pay=" + pay + "]";
+        }
+    }
+}
