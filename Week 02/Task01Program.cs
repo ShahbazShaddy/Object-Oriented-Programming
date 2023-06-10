219f2dd42779254b87155d01d198e6619a9b3f14
Shahbaz Ghafil <121121677+ShahbazShaddy@users.noreply.github.com>
2023-06-10 01:54:21 -0700

    Add files via upload

---
+++ Week 02/Task01Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lab02a
+{
+    internal class Program
+    {
+        class Student
+        {
+            public string name;
+            public int roll_num;
+            public float cgpa;
+        }
+        static void Main(string[] args)
+        {
+            Student student = new Student();
+            student.name = "ali";
+            student.roll_num = 10;
+            student.cgpa = 3.59F;
+            Console.WriteLine( "name:"+student.name+"\t"+"roll num:"+student.roll_num+"\t"+"cgpa:"+student.cgpa);
+            Console.ReadKey();
+        }
+    }
+}
