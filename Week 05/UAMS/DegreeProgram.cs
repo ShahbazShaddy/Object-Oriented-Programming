40318db57f31d8cba6f46ce9c8f5e8ad00059d63
Shahbaz Ghafil <121121677+ShahbazShaddy@users.noreply.github.com>
2023-06-10 06:39:23 -0700

    Add files via upload

---
+++ Week 05/UAMS/DegreeProgram.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAMS
+{
+    internal class DegreeProgram
+    {
+        public string degreeName;
+        public float degreeDuration;
+        public List<Subject> subjects;
+        public int seats;
+        public DegreeProgram(string degreeName, float degreeDuration, int seats)
+        {
+            this.degreeName = degreeName;
+            this.degreeDuration = degreeDuration;
+            this.seats = seats;
+            subjects = new List<Subject>();
+        }
+        public bool isSubjectExists(Subject sub)
+        {
+            foreach (Subject s in subjects)
+            {
+                if (s.code == sub.code)
+                {
+                    return true;
+                }
+            }
+            return false;
+        }
+        public bool AddSubject(Subject s)
+        {
+            int creditHours = calculateCreditHours();
+            if (creditHours + s.creditHours <= 20)
+            {
+                subjects.Add(s);
+                return true;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        public int calculateCreditHours()
+        {
+            int count = 0;
+            for (int x = 0; x < subjects.Count; x++)
+            {
+                count = count + subjects[x].creditHours;
+            }
+            return count;
+        }
+    }
+}
---
+++ Week 05/UAMS/Subject.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UAMS
+{
+    internal class Subject
+    {
+        public string code;
+        public string type;
+        public int creditHours;
+        public int subjectFees;
+
+        public Subject(string code, string type, int creditHours, int subjectFees)
+        {
+            this.code = code;
+            this.type = type;
+            this.creditHours = creditHours;
+            this.subjectFees = subjectFees;
+        }
+    }
+}
