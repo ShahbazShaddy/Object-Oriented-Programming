59e782a1dd3d29b3e7e138f98cd3576772dc11ca
Shahbaz Ghafil <121121677+ShahbazShaddy@users.noreply.github.com>
2023-06-10 01:14:10 -0700

    Add files via upload

---
+++ Week 01/Task02Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PD01b
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rnd = new Random();
+            int min = 0;
+            int max = 100;
+            int randomNumber = rnd.Next(min, max);
+            Console.WriteLine(randomNumber);
+            Console.ReadKey();
+        }
+    }
+}
