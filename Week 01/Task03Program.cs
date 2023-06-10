b3a995022dc7a036b3e66c9407a4031334220bac
Shahbaz Ghafil <121121677+ShahbazShaddy@users.noreply.github.com>
2023-06-10 01:15:01 -0700

    Add files via upload

---
+++ Week 01/Task03Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PD01c
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("I am message one without color");
+            Console.ForegroundColor = ConsoleColor.Red;
+            Console.WriteLine("I am red color");
+            Console.ForegroundColor = ConsoleColor.White;
+            Console.BackgroundColor = ConsoleColor.Green;
+            Console.WriteLine("I am an example with background color");
+            Console.BackgroundColor = ConsoleColor.Black;
+            Console.ReadKey();
+        }
+    }
+}
